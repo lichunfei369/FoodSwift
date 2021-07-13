32d5f2c5b70600375507241a12e99a83058746d8
Apple <lichunfeigood@gmail.com>
2021-07-13 17:06:08 +0800

    提交

--- FoodSwift/Main/Commun/CommunityVC.swift
+++ FoodSwift/Main/Commun/CommunityVC.swift
@@ -12,7 +12,6 @@ class CommunityVC: BasicController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     //重写父类方法
